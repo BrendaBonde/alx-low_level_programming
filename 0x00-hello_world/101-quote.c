f691bc4038e5e0d4c74f0b7504080c1960a31bb3
root <jepbonde@gmail.com>
2022-03-11 04:55:38 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
@@ -7,6 +6,7 @@
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	
+	write(2, ch, 59); 
 	return (1);
 }
