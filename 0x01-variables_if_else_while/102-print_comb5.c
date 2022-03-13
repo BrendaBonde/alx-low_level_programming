068b5622c4ca1575d38e882508ca9207d048f997
root <jepbonde@gmail.com>
2022-03-13 10:59:35 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Prints all possible combinations of two-digit numbers,
+ * ranging from 00-99, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int num1, num2;
