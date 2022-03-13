27316314f759dabe578ef801a0c55a06b82df208
root <jepbonde@gmail.com>
2022-03-13 05:15:26 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	fot (i = 48; i < 58; i++)
+	for (i=48; i < 58; i++)
 	{
 		putchar(i);
-		if (i != 57)
+		if (i !=57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar(',');
-	putchar(' ');
-}
-{
 	putchar('\n');
 	return (0);
 }
