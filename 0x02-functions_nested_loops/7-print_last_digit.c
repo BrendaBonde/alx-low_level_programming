9f93ab46b7c7efbc339953ba054b91e844e4754a
root <jepbonde@gmail.com>
2022-03-16 05:30:44 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_Last_digit - prints last digit
+ * print_last_digit - prints last digit
  *
  * @i: digit to be tested
  *
  * Return: value of the digit
  */
-int print_Last_digit(int i)
+int print_last_digit(int i)
 {
 	int m;
 
