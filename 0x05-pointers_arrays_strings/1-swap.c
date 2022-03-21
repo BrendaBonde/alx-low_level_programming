6d4f9c96b9ae7b8deb5534efde70634047b9909a
root <jepbonde@gmail.com>
2022-03-21 14:39:20 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps integer values
+ * @i: first value pointer
+ * @j: second value pointer
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*i = *i + *j;
+	*j = *i + *j;
+	*i = *i - *j;
+}
