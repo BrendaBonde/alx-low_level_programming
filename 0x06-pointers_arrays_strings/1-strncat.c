acf5e16e87fdea0d72ab1056ae1e242ed54d3d00
root <jepbonde@gmail.com>
2022-03-24 00:52:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int c1 = 0, c2 = 0;
 
-	while (*(dst + C1) != '\0')
+	while (*(dest + C1) != '\0')
 	{
 		C1++;
 	}
 
-	WHILE (C2 < n)
+	while (C2 < n)
 	{
 		*(dest + c1) = *(src + c2);
 		if (*(src + c2) == '\0')
