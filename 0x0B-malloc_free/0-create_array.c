2ad1b2e41c073e513e67b9de1ff1983fd9fc3ce6
root <jepbonde@gmail.com>
2022-04-07 03:00:32 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *p = (char *)malloc(size * size of(char));
+	char *p = (char *)malloc(size * sizeof(char));
 	unsigned int i = 0;
 
 	if (size == 0 || p == NULL)
