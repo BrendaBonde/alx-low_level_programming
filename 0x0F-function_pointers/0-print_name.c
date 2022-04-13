34239cacdfa29cf2df8742be0bad22832c949646
root <jepbonde@gmail.com>
2022-04-13 05:16:31 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - calls back a function
+ * @name: name of person
+ * @f: function pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
