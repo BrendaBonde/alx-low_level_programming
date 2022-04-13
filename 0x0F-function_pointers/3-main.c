9b1e8962ca51da0531804e2edc44ba5a0a0551f1
root <jepbonde@gmail.com>
2022-04-13 10:12:21 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - calls the calculator operations
+ * @argc: arguement counter
+ * @argv: arguement store
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operatio)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	n = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
+}
