ef6183b021684dcd12b21f4e75278d2a0e309811
root <jepbonde@gmail.com>
2022-04-13 05:44:19 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- * print_name - calls back a function
- * @name: name of person
+ * print_name - calls back to a function
+ * @name:name of person
  * @f: function pointer
  * Return: nothing
  */
-void print_name(char *name, void (*f)(char *))
+void print_name (char *name, void (*f)(char *))
 {
-	if (name != NULL && *name != '\0' && f != NULL)
+	if (name && f)
 		f(name);
 }
