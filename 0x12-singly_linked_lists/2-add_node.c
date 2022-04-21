eb334d00ceca94ecd97f2764927836f77b4be380
root <jepbonde@gmail.com>
2022-04-21 07:58:36 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,47 +1,29 @@
 #include "lists.h"
-#include <stdlib.h>
+#include <string.h>
 /**
- * _strlen - gets lenth of the string
- * @s: lenth of the string
- */
-int strlen(const char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
-/**
- * add_node - adds new nodes to the list
- * @head: current place in the list
- * @str: strin o add to the head
- * Return: pointer to current position in list
+ * add_node - adds new node to list
+ * @head: pointer to pointer to list beginning
+ * @str: a string in the node
+ * Return: address of new element, NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int i, len;
-	char *content;
-	list_t *new;
+	list_t *newNode;
 
-	if (str == NULL || head == NULL)
+	if (str == NULL)
 		return (NULL);
-	len = _strlen(str);
-	new = *head;
-	content = malloc((len + 1) * sizeof(char));
-	if (content == NULL)
+	newNode = (list_t *)malloc(sizeof(list_t));
+	if (newNode == NULL)
 		return (NULL);
-	for (i = 0; str[i]; i++)
-		content[i] = str[i];
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	newNode->str = strdup(str);
+	if (newNode->str == NULL)
 	{
-		free(content);
-		return (NULL);
+		free(newNode);
+		returnn (NULL);
 	}
-	new->str = content;
-	new-> = len;
-	new->next = *head;
-	*head = new;
-	return (new);
+	newNode->len = strlen(str);
+	newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
 }
