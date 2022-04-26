1ca96752cb3c4462cbb709290c13723b20fd184b
root <jepbonde@gmail.com>
2022-04-26 03:30:23 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H_
 #define LISTS_H_
-/** struct list_s - singly linked list
+/**
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
