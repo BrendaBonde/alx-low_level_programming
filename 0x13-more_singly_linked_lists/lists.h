1b9645e73bc705db40a32e267b06aacf666e6dbb
root <jepbonde@gmail.com>
2022-04-26 03:44:05 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -41,7 +41,7 @@ void before_main(void) __attribute__((constructor));
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **had, const int n);
+listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
