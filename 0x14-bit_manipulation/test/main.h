6f7f458dd9e5a0dfd2c94d1db6a51007e0ab35fc
root <jepbonde@gmail.com>
2022-04-29 09:08:21 -0700

    main.h

--- 0x14-bit_manipulation/test/main.h
+++ 0x14-bit_manipulation/test/main.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-char _putchar(char *)
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_endianness(void);
 
 #endif
