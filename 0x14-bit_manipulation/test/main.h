0b8d41aec28b3587e7cc0d24803fe51617422f3c
root <jepbonde@gmail.com>
2022-04-29 08:34:14 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/test/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char _putchar(char *)
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif
