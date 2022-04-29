3c54a4de84a57def74951956e2127d8f3c34f87c
root <jepbonde@gmail.com>
2022-04-29 09:10:18 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,9 @@ int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 unsigned long int _pow(unsigned int base, unsigned int power);
-
+int get_bit(unsigned int base, unsigned int index);
+int get_endianness(void);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif
