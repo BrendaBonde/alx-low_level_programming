17154638cbf3aaed0faa2984acc82500633506ab
root <jepbonde@gmail.com>
2022-05-03 09:43:50 -0700

    main.h

--- 0x15-file_io/test/main.h
+++ 0x15-file_io/test/main.h
@@ -5,6 +5,6 @@
 int _putchar(void);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-size_t _strlen(char *c);
+size_t _strlen(char *);
 
 #endif
