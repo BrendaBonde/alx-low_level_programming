df9e8069d215248403c649e4f87b93735add0d7e
root <jepbonde@gmail.com>
2022-05-03 09:32:02 -0700

    main.h

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,35 +5,41 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
- * create_file - creates a file
- * standard output.
- * @filename: name of the file to create.
- * @test_content: NULL terminated string to write to the file.
- * Return: 1 on success, -1 on failure.
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the strinf
+ */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+/**
+ * create_file - creates file
+ * @filename: pointer to file
+ * @text_content: contents in the file
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, lenght;
-	ssize_t res_write;
+	int fd;
+	ssize_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-	{
-		lenght = 0;
-		while (*(text_content + lenght) != '\0')
-			lenght++;
-		res_write = write(fd, text_content, lenght);
-		if (res_write == -1)
-		{
-			write(1, "fails", 6);
-			return (-1);
-		}
-	}
+		len = write(fd, text_content. _strlen(text_content));
 	close(fd);
+	if (len == -1)
+		return (-1);
 	return (1);
 }
