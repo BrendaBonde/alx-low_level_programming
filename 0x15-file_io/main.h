167239b4877aa4688086f349bb9e0791ff30abec
root <jepbonde@gmail.com>
2022-05-04 04:17:05 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-#include <stdio.h>
 
-int _putchar(void);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-size_t _strlen(char *);
-int append_text_to_file(const char *filename, char *text_content):
-void error_file(int file_from, int file_to, char *argv[]);
-int main(int argc, char *argv[]);
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif
